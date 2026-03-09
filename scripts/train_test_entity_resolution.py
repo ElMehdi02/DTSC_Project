16e3e8e30076d8f4c34286feeae741d621279da6
ElMehdi02 <bensoudamehdi6.com>
2026-03-09 12:42:34 -0400

    DTSC_project_ElMehdiBensouda

---
+++ scripts/train_test_entity_resolution.py
@@ -0,0 +1,23 @@
+from merged_data import MergedData
+from features_resolutions import EntityResolutionFeatures
+from classifiers.resolution_classifier import EntityResolutionClassifier
+
+
+def train():
+    # Add your training data here
+    erc = entity_resolution_classifier.EntityResolutionClassifier()
+    erc.train(features, labels)
+    erc.save("data/erc.model")
+
+
+def test():
+    erc = entity_resolution_classifier.EntityResolutionClassifier()
+    erc.load("data/erc.model")
+
+    feature_model = entity_resolution_features.EntityResolutionFeatures()
+    md = merged_data.MergedData()  # From the database!
+    for batch in md.get_merged_data():
+        features = feature_model.features(batch)
+        is_match = erc.predict(features)
+
+        # To the database!
